118e9553cbd0bf3613f5166862d41048064f687e
Igor <igormedeiros407@gmail.com>
2024-10-17 03:20:22 -0300

    commit 2

---
+++ src/main/java/desafioQuatro/DesafioSenha.java
@@ -0,0 +1,17 @@
+package desafioQuatro;
+import java.util.Scanner;
+public class DesafioSenha {
+    public static void main(String[] args) {
+    java.util.Scanner leituraNome = new Scanner(System.in);
+    System.out.println("Digite seu nome");
+    String nome = leituraNome.nextLine();
+    System.out.println("Digite sua senha: ");// senha tem que ser == 123;
+    int senha = leituraNome.nextInt();
+    if (senha == 123){
+        System.out.println("Sr "+ nome + " sua senha está correta");
+    }else{
+        System.out.println("cliente "+ nome + " sua senha Invalida");
+    }
+
+    }
+}
