2b1e59f08479d9f2cfca7ca0c825ca99fa4d262c
Igor <igormedeiros407@gmail.com>
2024-11-17 22:33:07 -0300

    atualizando estudos 2

---
+++ src/main/java/lacoRepeticao/Dowhile.java
@@ -0,0 +1,14 @@
+package lacoRepeticao;
+
+public class Dowhile {
+    public static void main(String[] args) {
+        int contador = 10;
+
+        do {
+            System.out.println("numero:" + contador) ;
+            contador--;
+        }while (contador >= 1);
+
+
+    }
+}
---
+++ src/main/java/lacoRepeticao/ValidandoSenha.java
@@ -0,0 +1,21 @@
+package lacoRepeticao;
+
+public class ValidandoSenha {
+    /*  // Objetivo \\
+    Peça ao usuário para digitar uma senha.
+    Valide se a senha é igual a "1234".
+    Caso a senha esteja incorreta, exiba uma mensagem como "Senha inválida! Tente novamente." e peça para o usuário digitar novamente.
+    Continue pedindo até que o usuário acerte a senha.
+    Quando a senha estiver correta, exiba a mensagem "Acesso permitido." e encerre o programa.
+    Requisitos:
+    Utilize o laço do-while.
+    Use a classe Scanner para receber a entrada do usuário.
+    A validação deve acontecer após o primeiro pedido de senha (garantindo que o bloco do do execute pelo menos uma vez).
+    */
+    public static void main(String[] args) {
+
+
+
+        
+    }
+}
